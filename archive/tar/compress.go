32dbfd24b732c8efe6ba821944771c8f210dcfc1
Frantjc <logsdonalex11@gmail.com>
2024-10-19 22:07:22 -0400

    ensure close

--- archive/tar/compress.go
+++ archive/tar/compress.go
@@ -15,47 +15,47 @@ func Compress(dir string) io.ReadCloser {
 		tw := tar.NewWriter(pw)
 		defer tw.Close()
 
-		if err := filepath.WalkDir(dir, func(path string, di fs.DirEntry, err error) error {
-			if err != nil {
-				return err
-			}
-
-			fi, err := di.Info()
-			if err != nil {
-				return err
-			}
-
-			hdr, err := tar.FileInfoHeader(fi, fi.Name())
-			if err != nil {
-				return err
-			}
-
-			rel, err := filepath.Rel(dir, path)
-			if err != nil {
-				return err
-			}
-			hdr.Name = rel
-
-			if err := tw.WriteHeader(hdr); err != nil {
-				return err
-			}
-
-			if !fi.Mode().IsDir() {
-				file, err := os.Open(path)
+		_ = pw.CloseWithError(
+			filepath.WalkDir(dir, func(path string, di fs.DirEntry, err error) error {
 				if err != nil {
 					return err
 				}
-				defer file.Close()
 
-				if _, err := io.Copy(tw, file); err != nil {
+				fi, err := di.Info()
+				if err != nil {
 					return err
 				}
-			}
 
-			return nil
-		}); err != nil {
-			_ = pw.CloseWithError(err)
-		}
+				hdr, err := tar.FileInfoHeader(fi, fi.Name())
+				if err != nil {
+					return err
+				}
+
+				rel, err := filepath.Rel(dir, path)
+				if err != nil {
+					return err
+				}
+				hdr.Name = rel
+
+				if err := tw.WriteHeader(hdr); err != nil {
+					return err
+				}
+
+				if !fi.Mode().IsDir() {
+					file, err := os.Open(path)
+					if err != nil {
+						return err
+					}
+					defer file.Close()
+
+					if _, err := io.Copy(tw, file); err != nil {
+						return err
+					}
+				}
+
+				return nil
+			}),
+		)
 	}()
 
 	return pr
